1369369b4a436060249cbc4218dfbc63eed6ea43
Eric Berglund <eric.berglund@netlight.com>
2018-04-23 22:30:14 +0200

    Add lambda and tests

---
+++ handlers/hello_handler.py
@@ -0,0 +1,2 @@
+def handle_event(event):
+    return 'Hello {}'.format(event['name'])
---
+++ tests/hello_handler_test.py
@@ -0,0 +1,20 @@
+import unittest
+from handlers.hello_handler import handle_event
+
+
+class TestHelloHandler(unittest.TestCase):
+
+    def test_string(self):
+        self.assertEqual(handle_event({'name': 'Eric'}), 'Hello Eric')
+
+    def test_non_dict(self):
+        with self.assertRaises(TypeError):
+            handle_event(1)
+
+    def test_no_name_key(self):
+        with self.assertRaises(KeyError):
+            handle_event(dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
